67ccacf8e506ed905c730edecef3f9cff3e8db8e
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2020-10-04 22:18:06 -0500

    feature(controllers): Sessions delete and put

--- src/main/java/com/guillermo/conferencedemo/controllers/SessionsController.java
+++ src/main/java/com/guillermo/conferencedemo/controllers/SessionsController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.guillermo.conferencedemo.models.Session;
 import com.guillermo.conferencedemo.repositories.SessionRepository;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,7 +30,7 @@ public class SessionsController {
 
   @GetMapping
   @RequestMapping("{id}")
-  public Session get(@PathVariable Long id){
+  public Session get(@PathVariable Long id) {
     return sessionRepository.getOne(id);
   }
 
@@ -36,6 +38,23 @@ public class SessionsController {
   @ResponseStatus(HttpStatus.CREATED)
   public Session create(@RequestBody final Session session) {
     return sessionRepository.saveAndFlush(session);
+  }
+
+  @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+  public void delete(@PathVariable Long id) {
+    // TODO: Figure out how to also check for children records before deleting.
+    sessionRepository.deleteById(id);
+  }
+
+  @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+  public Session update(@PathVariable Long id, @RequestBody Session session) {
+    // because this PUT, we expect all attributes to be passed in.
+    // PATCH, would only need what is changed
+    // TODO: Add Validation that all attributes are passed in, otherwise return 400 bad request
+
+    Session existingSession = sessionRepository.getOne(id);
+    BeanUtils.copyProperties(session, existingSession, "session_id");
+    return sessionRepository.saveAndFlush(existingSession);
   }
 
 }
